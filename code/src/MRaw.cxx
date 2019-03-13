0d7a7c0902d9bcb852862f11af9a62cfb80951c7
Schotter Romain <romain.schotter@gmail.com>
2019-03-13 10:17:30 +0100

    MRaw

--- code/src/MRaw.cxx
+++ code/src/MRaw.cxx
@@ -93,6 +93,7 @@
 #include <iomanip>
 #include <assert.h>
 #include <vector>
+#include <cmath>
 
 #ifdef USETMVA
 #include "TMVA/Factory.h"
@@ -16103,21 +16104,30 @@ void MRaw::SitrineoAnalysis( Int_t lastPlaneOfFirstTracker, Int_t &nPairs, track
 
   // Loop on all tracks and pair them
   nPairs = 0;
-  for( Int_t iTrack=1; iTrack<=tTracker->GetTracksN(); iTrack++) { // loop on 1st tracks
+  for( Int_t iTrack=1; iTrack<=tTracker->GetTracksN(); iTrack++) { // loop on 1st tracks // recup t from 1st tracker
     track1 = (DTrack*)tTracker->GetTrack( iTrack);
+    int plane1 = track1->GetHit(0)->GetPlane()->GetPlaneNumber();
     slope1 = track1->GetLinearFit().GetSlopeZ();
 
     for( Int_t jTrack=iTrack+1; jTrack<=tTracker->GetTracksN(); jTrack++) { // loop on 2nd tracks
       track2 = (DTrack*)tTracker->GetTrack( jTrack);
-      slope2 = track2->GetLinearFit().GetSlopeZ();
+      int plane2 = track2->GetHit(0)->GetPlane()->GetPlaneNumber(); 
+      if( plane2 < plane1){     
+	cout << "First Track " << plane1 << endl ;  
+        cout << "Second Track " << plane2 << endl ;    
+      	slope2 = track2->GetLinearFit().GetSlopeZ();
 
-      pairList[nPairs].firstTrackID = iTrack;
-      pairList[nPairs].secondTrackID = jTrack;
-      pairList[nPairs].slope1 = slope1(0);
-      pairList[nPairs].slope2 = slope2(0);
-      pairList[nPairs].momentumXY = pairList[nPairs].slope2-pairList[nPairs].slope1 ;
-      nPairs++;
-
+      	pairList[nPairs].firstTrackID = iTrack;
+      	pairList[nPairs].secondTrackID = jTrack;
+      	pairList[nPairs].slope1 = slope1(0);
+      	pairList[nPairs].slope2 = slope2(0);
+      	double angle = acos( ( (pairList[nPairs].slope2)*(pairList[nPairs].slope1) + 1 )/(sqrt(1+pow(pairList[nPairs].slope2,2) ) * sqrt(1+pow(pairList[nPairs].slope2,2) ) ) ) ;
+	cout << " Angle : " << angle << endl ;
+	double momentum = 0.3*0.2*20/angle ;
+	pairList[nPairs].momentumXY = momentum ;
+	cout << " Momentum : " << momentum << endl ;
+      	nPairs++;
+      }
     } // end loop on 2nd tracks
 
   } // end loop on 1st tracks
